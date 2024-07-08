0ccdc9181587adbad89e2e6beff6f302816ad593
ParsaAminpour <parsa.aminpour@gmail.com>
2024-07-08 16:02:51 +0330

    the ground movement bug resolved

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use crossterm::{
 use ndarray::{Array2, ArrayBase, Dim, OwnedArcRepr, Array};
 use inline_colorization::*;
 use rand::prelude::*;
+use chrono::prelude::*;
 use std::time::Duration;
 
 /*
@@ -15,14 +16,15 @@ use std::time::Duration;
 * physics: performing the physical changes for the boats or the enemies
 * drawing: performing the changes related to the screen.
 */
-
 trait GameStructure {
     // comprehensive explanation
     fn draw(&mut self, screen: &mut Stdout) -> Result<()>;
     // comprehensive explanation
-    fn reactions(&mut self, screen: &mut Stdout) -> Result<&mut Game2DMatrix>;
+    fn reactions(&mut self, /*screen: &mut Stdout*/) -> Result<&mut Game2DMatrix>;
     // comprehensive explanation
     fn initialize_ground(&mut self, screen: &mut Stdout) -> Result<&mut Self>;
+    // comprehensive explanation
+    fn shyft_ground_loc(&mut self, change: bool) -> Result<&mut Self>;
 }
 
 
@@ -37,6 +39,7 @@ pub struct Game2DMatrix {
     player_j: u16,
     max_screen_i: u16,
     max_screen_j: u16,
+    screen_mid: u16,
     map: Array2<f64>,
     ground: Vec<(u16, u16)>,
     game_staus: GameStatus,
@@ -45,29 +48,76 @@ pub struct Game2DMatrix {
 
 
 impl GameStructure for Game2DMatrix {
-    fn draw(&mut self, screen: &mut Stdout) -> Result<()> {
-        // draw the map as first scence
+    /// @notice this function will use at the beginning of the game to initialize the ground borders.
+    /// @dev this function will use in the draw function is self.initialize was false.
+    fn initialize_ground(&mut self, screen: &mut Stdout) -> Result<&mut Self> {
+        // initial phase of screen
+        screen.queue(Clear(ClearType::All))?;
+
+        let mut rng = rand::thread_rng();
+        // let screen_mid = self.max_screen_i / 2;
+        let mut lg_range: u16;
+        let mut rg_range: u16;
+        let mut change_precision = rng.gen_range(5..10);
+
         for i in 0..self.map.row(0).len() {
-            screen.queue(MoveTo(1, i as u16))?; // (i, j)
+            if i % change_precision == 0 {
+                lg_range = rng.gen_range((self.screen_mid - 40)..self.screen_mid);
+                rg_range = rng.gen_range(self.screen_mid..(self.screen_mid + 40));
+
+                (self.ground[i].0, self.ground[i].1) = (lg_range, rg_range);
+                change_precision = rng.gen_range(change_precision..15);
+
+            } else {
+                (self.ground[i].0, self.ground[i].1) = (self.ground[i - 1].0, self.ground[i - 1].1);
+            }
+        }
+
+        self.initialized = true;
+        Ok(self)
+    }
+
+    fn draw(&mut self, screen: &mut Stdout) -> Result<()> {
+        screen.queue(Clear(ClearType::All))?;
+
+        // draw the map as first scence
+        for i in 0..(self.map.row(0).len()) {
+            screen.queue(MoveTo(0, i as u16))?; // (i, j)
             screen.queue(Print("+".repeat(self.ground[i].0 as usize)))?;
 
             screen.queue(MoveTo(self.ground[i].1, i as u16))?;
             screen.queue(Print("+".repeat((self.max_screen_i - self.ground[i].1) as usize)))?;
-
-            if i > 1 { self.ground[i-1] = self.ground[i]; }
         }
 
         // draw the player
         screen.queue(MoveTo(self.player_i, self.player_j))?;
         screen.queue(Print("p"))?;
         
-        // @audit-info invalid flushing
         screen.flush()?;            
         Ok(())
     }
 
+    fn shyft_ground_loc(&mut self, change: bool) -> Result<&mut Self> {
+        for i in (1..self.map.row(0).len()).rev() {
+            self.ground[i] = self.ground[i - 1];
+        }
+        
+        if change {
+            let mut rng = rand::thread_rng();
 
-    fn reactions(&mut self, screen: &mut Stdout) -> Result<&mut Self> {
+            let delta = rng.gen_range(5..self.screen_mid);
+            let h_move = rng.gen_range(0..(self.screen_mid));
+            self.ground[0] = ((self.screen_mid - delta), (self.screen_mid + delta));
+            Ok(self)
+
+        } else {
+            self.ground[0] = self.ground[1];
+            Ok(self)
+        }
+    }
+
+
+    fn reactions(&mut self, /*screen: &mut Stdout*/) -> Result<&mut Self> {
         let user_j: usize = self.player_j as usize;
 
         // handling the boat accidentation with ground
@@ -77,58 +127,14 @@ impl GameStructure for Game2DMatrix {
 
         Ok(self)
     }
-
-    fn initialize_ground(&mut self, screen: &mut Stdout) -> Result<&mut Self> {
-        // initial phase of screen
-        screen.queue(Clear(ClearType::All))?;
-
-        for i in 0..self.map.row(0).len() {
-            let mut rng = rand::thread_rng();
-
-            if i == 0 {
-                let low = self.player_i - 30;
-                let high = self.player_i + 30;
-                let rand_loc: u16 = rng.gen_range(low..high);
-                (self.ground[i].0, self.ground[i].1) = (low, high);
-            }
-            
-            screen.queue(MoveTo(1, i as u16))?; // (i, j)
-            screen.queue(Print("+".repeat(rand_loc as usize)))?;
-
-            screen.queue(MoveTo(self.ground[i].1, i as u16))?;
-            screen.queue(Print("+".repeat((self.max_screen_i - rand_loc) as usize)))?;
-        }
-
-        self.initialized = true;
-        Ok(self)
-    }
-
-    // fn change_the_ground_border(&mut self, screen: &mut Stdout) -> Result<&mut Self>{
-    //     let user_j: usize = self.player_j as usize;
-
-    //     // changing the border of the ground
-    //     let mut rand = rand::thread_rng();
-    //     let rand_number: u16 = rand.gen();
-
-    //     if rand_number < self.ground[user_j].0 {
-    //         self.ground[1].0 -= 1 ;
-    //         self.ground[1].1 -= 1;
-    //     } else if rand_number > self.ground[1].1 {
-    //         self.ground[1].0 += 1;
-    //         self.ground[1].1 += 1;
-    //     }
-    //     self.ground[2] = self.ground[1];
-    //     self.ground[0] = (self.ground[1].0, self.ground[1].1);
-
-    //     Ok(self)
-    // }
 }
+
 
 
 fn main() -> Result<()> {
     let mut screen = stdout();
     enable_raw_mode().unwrap();
-    screen.execute(Hide).unwrap();
+    // screen.execute(Hide).unwrap();
 
     
     // initialize the game information
@@ -136,23 +142,31 @@ fn main() -> Result<()> {
 
     let mut nd2array = &mut Game2DMatrix {
         player_i: max_i / 2,
-        player_j: max_j / 2,
+        player_j: max_j - 10,
         max_screen_i: max_i, 
         max_screen_j: max_j,
+        screen_mid: max_i / 2,
         map: Array::from_shape_vec(
             (max_i as usize, max_j as usize), 
             vec![0.0; (max_i*max_j) as usize]).unwrap(),
-        ground: vec![((max_i / 2) - 10, (max_i / 2) + 10); max_j as usize],
+        // ground: vec![((max_i / 2) - 10, (max_i / 2) + 10); max_j as usize],
+        ground: vec![(0,0); max_j as usize],
         game_staus: GameStatus::ALIVE,
         initialized: false
     };
 
-    nd2array = nd2array.initialize_ground(&mut screen).unwrap();
-
+    nd2array = nd2array.initialize_ground(&mut screen)?;
+    
     while nd2array.game_staus == GameStatus::ALIVE {
+        let now = Utc::now().timestamp();
+
         // implementing the keyboard binding.
         if poll(Duration::from_millis(10))? {
-            let key: Event = read().unwrap();
+
+            let key = read().unwrap();
+            while poll(Duration::from_millis(0)).unwrap() {
+                let _ = read();
+            }
             match key {
                 Event::Key(event) => {
                     match event.code {
@@ -185,17 +199,16 @@ fn main() -> Result<()> {
                 _ => {}
             }
         }
-
         sleep(Duration::from_millis(100));
-        nd2array = nd2array.reactions(&mut screen).unwrap();
-        // nd2array = nd2array.change_the_ground_border(&mut screen).unwrap();
         nd2array.draw(&mut screen).unwrap();
+        nd2array = nd2array.shyft_ground_loc(rand::thread_rng().gen_bool(0.05)).unwrap();
+        nd2array = nd2array.reactions().unwrap();
 
         if nd2array.game_staus == GameStatus::DEATH { break; }
     }
 
-    screen.execute(Show)?;
     screen.flush().unwrap();
+    screen.execute(Show)?;
     screen.queue(MoveTo(nd2array.max_screen_i / 2, 0))?;
     screen.queue(Print(format!("{color_green}Thanks for playing{color_reset}\n")))?;
 
@@ -203,3 +216,33 @@ fn main() -> Result<()> {
 }
     
 
+
+// fn main() {
+//     let mut screen = stdout();
+//     enable_raw_mode().unwrap();
+//     // screen.execute(Hide).unwrap();
+
+    
+//     // initialize the game information
+//     let (max_i, max_j) = size().unwrap();
+
+//     let mut nd2array = &mut Game2DMatrix {
+//         player_i: max_i / 2,
+//         player_j: max_j - 10,
+//         max_screen_i: max_i, 
+//         max_screen_j: max_j,
+//         map: Array::from_shape_vec(
+//             (max_i as usize, max_j as usize), 
+//             vec![0.0; (max_i*max_j) as usize]).unwrap(),
+//         // ground: vec![((max_i / 2) - 10, (max_i / 2) + 10); max_j as usize],
+//         ground: vec![(0,0); max_j as usize],
+//         game_staus: GameStatus::ALIVE,
+//         initialized: false
+//     };
+
+//     nd2array = nd2array.initialize_ground(&mut screen).unwrap();
+
+//     for i in 0..nd2array.ground.len() {
+//         println!("{:?}\n", nd2array.ground[i]);
+//     }
+// }
