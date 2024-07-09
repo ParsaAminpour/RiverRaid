1bbbc21489e8ad1eaac2754093cb6b29cecd550c
ParsaAminpour <parsa.aminpour@gmail.com>
2024-07-09 16:36:00 +0330

    Enemies and Bullets have been added to the game and work correctly

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{borrow::BorrowMut, io::{stdout, Result, Stdout, Write}, thread::sleep};
+use std::{fs::ReadDir, io::{stdout, Result, Stdout, Write}, thread::sleep};
 use crossterm::{
     cursor::{Hide, MoveTo, Show}, event::{self, poll, read, Event, KeyCode}, execute, style::{
         Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor}, 
@@ -18,7 +18,7 @@ use std::time::Duration;
 */
 trait GameStructure {
     // comprehensive explanation
-    fn draw(&mut self, screen: &mut Stdout) -> Result<()>;
+    fn draw(&mut self, screen: &mut Stdout, resolve_enemy: bool) -> Result<()>;
     // comprehensive explanation
     fn reactions(&mut self, /*screen: &mut Stdout*/) -> Result<&mut Game2DMatrix>;
     // comprehensive explanation
@@ -30,7 +30,23 @@ trait GameStructure {
 
 #[derive(Debug, PartialEq, Eq)]
 enum GameStatus {
-    ALIVE, DEATH, PAUSED, FUEL_ENDED
+    ALIVE, DEATH, /*PAUSED, FUEL_ENDED*/
+}
+
+#[derive(Debug)]
+struct Location {
+    element_i: u16,
+    element_j: u16
+}
+#[derive(Debug)]
+struct Enemy {
+    location: Location,
+    logo: String,
+}
+#[derive(Debug)]
+struct Bullet {
+    location: Location,
+    energy: u16
 }
 
 #[derive(Debug)]
@@ -42,6 +58,8 @@ pub struct Game2DMatrix {
     screen_mid: u16,
     map: Array2<f64>,
     ground: Vec<(u16, u16)>,
+    enemies: Vec<Enemy>,
+    bullets: Vec<Bullet>,
     game_staus: GameStatus,
     initialized: bool,
     logo: String,
@@ -77,39 +95,78 @@ impl GameStructure for Game2DMatrix {
         Ok(self)
     }
 
-    fn draw(&mut self, screen: &mut Stdout) -> Result<()> {
+
+    fn draw(&mut self, screen: &mut Stdout,  resolve_enemy: bool) -> Result<()> {
         screen.queue(Clear(ClearType::All))?;
 
         // draw the map as first scence
         for i in 0..(self.map.row(0).len()) {
             screen.queue(MoveTo(0, i as u16))? // (i, j)
+                .queue(SetForegroundColor(Color::Green))?
                 .queue(Print("+".repeat(self.ground[i].0 as usize)))?
                 .queue(MoveTo(self.ground[i].1, i as u16))?
-                .queue(Print("+".repeat((self.max_screen_i - self.ground[i].1) as usize)))?;
+                .queue(Print("+".repeat((self.max_screen_i - self.ground[i].1) as usize)))?
+                .queue(ResetColor)?;
+        }
+
+        for bullet in self.bullets.iter() {
+            screen.queue(MoveTo(bullet.location.element_j, bullet.location.element_i))?
+                .queue(Print("o"))?;
+        }
+
+        // adjust enemy in the posibility of 10% of situations
+        if resolve_enemy {
+            self.enemies.push( Enemy {
+                location: Location { 
+                    element_i: 2, 
+                    element_j: rand::thread_rng().gen_range(self.ground[2].0..self.ground[2].1)
+                },
+                logo: '👾'.to_string(),
+            })
+        }
+
+        for enemy in self.enemies.iter() {
+            screen.queue(MoveTo(enemy.location.element_j, enemy.location.element_i))?
+                .queue(SetForegroundColor(Color::Red))?
+                .queue(Print(&enemy.logo))?
+                .queue(ResetColor)?;
         }
 
         // draw the player
         screen.queue(MoveTo(self.player_i, self.player_j))?;
         screen.queue(Print(&self.logo))?;
         
-        screen.flush()?;            
+        screen.flush()?;               
         Ok(())
     }
 
+
+    /// @notice this function perform the elements' movement during the game loop i.e. bullets, enemies, etc.
+    /// @dev this function will be called after the draw function to get the modified nd2array game information.
     fn shift_ground_loc(&mut self, change: bool) -> Result<&mut Self> {
         for i in (1..self.map.row(0).len()).rev() {
             self.ground[i] = self.ground[i - 1];
         }
 
+        for bullet in self.bullets.iter_mut().rev() {
+            bullet.location.element_i = bullet.location.element_i.saturating_sub(2);
+        }
+        self.bullets.retain(|bullet| bullet.location.element_i >= 3);
+
+
+        // manipulate the existing enemies in the map.
+        for enemy in self.enemies.iter_mut() {
+            enemy.location.element_i = enemy.location.element_i.saturating_add(1);
+        }
+        
+
         let mut rng = rand::thread_rng();
         let delta = rng.gen_range(1..6);
 
-        // TODO: handle the ground border change more intelligently and less artificially.
         if change && (self.ground[1].1 < self.max_screen_i - 5) {
-            // self.ground[0] = ((self.screen_mid - delta), (self.screen_mid + delta));
             self.ground[0] = (self.ground[1].0 + delta, self.ground[1].1 + delta);
             Ok(self)
-        } else if self.ground[1].0 > 3 {
+        } else if self.ground[1].0 > delta {
             self.ground[0] = (self.ground[1].0 - delta, self.ground[1].1 - delta);
             Ok(self)
         } else {
@@ -119,15 +176,42 @@ impl GameStructure for Game2DMatrix {
     }
 
 
+
     fn reactions(&mut self, /*screen: &mut Stdout*/) -> Result<&mut Self> {
         let user_j: usize = self.player_j as usize;
 
         // handling the boat accidentation with ground
         if self.player_i <= self.ground[user_j].0 || self.player_i >= self.ground[user_j].1
-            // || (self.player_j == self.ground[user_j].0 || self.player_j == self.ground[user_j].)
         {
             self.game_staus = GameStatus::DEATH;
         }
+
+
+        let mut enemies_to_remove:Vec<usize> = vec![];
+
+        for (idx, enemy) in self.enemies.iter_mut().enumerate() {
+            if enemy.location.element_j == self.player_i && enemy.location.element_i == self.player_j {
+                self.game_staus = GameStatus::DEATH;
+            }
+
+            for bullet in self.bullets.iter_mut() {
+                if (enemy.location.element_i-3..enemy.location.element_i+3).contains(&bullet.location.element_i) &&
+                    (enemy.location.element_j-3..enemy.location.element_j+3).contains(&bullet.location.element_j)  
+                {                
+                    enemy.logo = ' '.to_string();
+                    enemies_to_remove.push(idx);
+                }
+            }
+        }
+        enemies_to_remove.sort_unstable_by(|a, b| b.cmp(a)); // Sort in reverse order
+        for idx in enemies_to_remove {
+            self.enemies.remove(idx);
+        }
+
+
+        self.enemies.retain(|enemy| {
+            enemy.location.element_i < self.max_screen_j - 3
+        });
 
         Ok(self)
     }
@@ -154,6 +238,8 @@ fn main() -> Result<()> {
             vec![0.0; (max_i*max_j) as usize]).unwrap(),
         // ground: vec![((max_i / 2) - 10, (max_i / 2) + 10); max_j as usize],
         ground: vec![(0,0); max_j as usize],
+        enemies: vec![],
+        bullets: vec![],
         game_staus: GameStatus::ALIVE,
         initialized: false,
         logo: '⛵'.to_string(),
@@ -162,8 +248,6 @@ fn main() -> Result<()> {
     nd2array = nd2array.initialize_ground(&mut screen)?;
     
     while nd2array.game_staus == GameStatus::ALIVE {
-        let now = Utc::now().timestamp();
-
         // implementing the keyboard binding.
         if poll(Duration::from_millis(10))? {
 
@@ -183,6 +267,16 @@ fn main() -> Result<()> {
                         KeyCode::Up => if nd2array.player_j - 1 > 0 { nd2array.player_j -= 1; },
 
                         KeyCode::Down => if nd2array.player_j + 1 < nd2array.max_screen_j { nd2array.player_j += 1; },
+
+                        KeyCode::Char(' ') => {
+                            nd2array.bullets.push(Bullet {
+                                location: Location {
+                                    element_i: nd2array.player_j,
+                                    element_j: nd2array.player_i,
+                                },
+                                energy: 10
+                            });
+                        }
                         _ => {}
                     }
                 },
@@ -191,7 +285,9 @@ fn main() -> Result<()> {
         }
         sleep(Duration::from_millis(100));
         nd2array = nd2array.reactions().unwrap();
-        nd2array.draw(&mut screen).unwrap();
+
+        nd2array.draw(&mut screen, rand::thread_rng().gen_bool(0.1)).unwrap();
+
         nd2array = nd2array.shift_ground_loc(rand::thread_rng().gen_bool(0.5)).unwrap();
 
         if nd2array.game_staus == GameStatus::DEATH { break; }
@@ -204,35 +300,3 @@ fn main() -> Result<()> {
 
     Ok(())
 }
-    
-
-
-// fn main() {
-//     let mut screen = stdout();
-//     enable_raw_mode().unwrap();
-//     // screen.execute(Hide).unwrap();
-
-    
-//     // initialize the game information
-//     let (max_i, max_j) = size().unwrap();
-
-//     let mut nd2array = &mut Game2DMatrix {
-//         player_i: max_i / 2,
-//         player_j: max_j - 10,
-//         max_screen_i: max_i, 
-//         max_screen_j: max_j,
-//         map: Array::from_shape_vec(
-//             (max_i as usize, max_j as usize), 
-//             vec![0.0; (max_i*max_j) as usize]).unwrap(),
-//         // ground: vec![((max_i / 2) - 10, (max_i / 2) + 10); max_j as usize],
-//         ground: vec![(0,0); max_j as usize],
-//         game_staus: GameStatus::ALIVE,
-//         initialized: false
-//     };
-
-//     nd2array = nd2array.initialize_ground(&mut screen).unwrap();
-
-//     for i in 0..nd2array.ground.len() {
-//         println!("{:?}\n", nd2array.ground[i]);
-//     }
-// }
